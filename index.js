ec0350f60769a68a626ebb7ba8d781559c2dfd49
Andrea Haveman <andhaveman@hotmail.com>
2019-02-26 04:40:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ musicianInstrument[i]=musicianString;
 return musicianInstrument;
 }
 
-function johnLennonFacts (){
+var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+function johnLennonFacts (facts){
   const facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
